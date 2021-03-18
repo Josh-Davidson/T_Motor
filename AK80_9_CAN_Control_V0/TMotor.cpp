ea47c052589f531d7a5da9d96421096a2b17e716
Josh-Davidson <79871009+Josh-Davidson@users.noreply.github.com>
2021-03-18 14:20:24 -0700

    Update TMotor.cpp

--- AK80_9_CAN_Control_V0/TMotor.cpp
+++ AK80_9_CAN_Control_V0/TMotor.cpp
@@ -5,3 +5,15 @@ Serial_CAN can;
 TMotor::TMotor(int ID) {
   id = ID;
 }
+double pos()
+{
+  Serial.print(p_out);
+}
+double vel()
+{
+  Serial.print(v_out);
+}
+double tor()
+{
+  Serial.print(i_out);
+}
